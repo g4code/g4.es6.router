6fe1f9c672e51778f59073eef0d9af8b1840407e
Zeljko Simic <zeljkosbgd@gmail.com>
2018-12-18 12:56:57 +0100

    Added variable placeholders

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Request from './request.js'
 
+
+
 export default class Router  {
 
     constructor () {
         this.routes = {}
-        this.currentPath = null;
+        this.currentPath;
     }
 
     add (options) {
@@ -14,7 +16,7 @@ export default class Router  {
     }
 
     destroyPrevRouteIfExist () {
-        let route = this.routes[this.currentPath]
+        let route = this.findRoute(this.currentPath)
         if (route !== undefined && route.runner !== undefined) {
             new route.runner().destroy()
         }
@@ -22,15 +24,16 @@ export default class Router  {
 
     run () {
         let request = new Request()
-        let path = request.getPath()
-        let route = this.routes[path]
+        let route = this.findRoute( request.getPath())
+        this.destroyPrevRouteIfExist()
+        this.runCurrentRoute(route, request)
+    }
 
+    runCurrentRoute (route, request) {
         if (route === undefined) {
             console.log(`Rout ${path} not exists`)
             return false;
         }
-
-        this.destroyPrevRouteIfExist()
 
         if (route.callback !== undefined) {
             route.callback.call(this, request)
@@ -40,7 +43,32 @@ export default class Router  {
             new route.runner().run(request)
         }
 
-        this.currentPath = path
+        this.currentPath = request.getPath()
+    }
+
+    findRoute (path) {
+        if (typeof path === 'string') {
+            let key = Object.keys(this.routes).find(route => this.matchPath(path, route))
+            return this.routes[key]
+        }
+    }
+
+
+    matchPath (locationPath, routePath) {
+        locationPath = locationPath.split('/')
+        routePath = routePath.split('/')
+
+        if(locationPath.length === routePath.length){
+            let isUriPath = true;
+            for(let key in locationPath) {
+                if(locationPath[key] !== routePath[key] && !/^:/.test( routePath[key])){
+                    isUriPath = false;
+                }
+            }
+            return isUriPath
+        }
+
+        return false;
     }
 
 }
